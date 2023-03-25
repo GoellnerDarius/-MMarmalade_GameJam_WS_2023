6a034fddfc80eceeb7342b5dbe158b0dde66c2ef
Göllner Darius <fhs49570@fh-salzburg.ac.at>
2023-03-25 16:21:43 +0100

    Updated Sprites, added movementlimit for player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 10f;
     public float upperBound =  0;
+    public float lowerBounds = -9;
+    public float leftBound= -17;
+    public float rightBound = 17;
     void Start()
     {
     }
@@ -23,22 +26,21 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 getMovement()
     {
+        float moveSpeed = (speed * Time.deltaTime);
         Vector3 movement = new(0, 0, 0);
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)&&transform.position.x-moveSpeed>leftBound)
             movement += new Vector3(-1, 0, 0);
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)&&transform.position.x+moveSpeed<rightBound)
             movement += new Vector3(1, 0, 0);
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)&&transform.position.y+moveSpeed<upperBound)
             movement += new Vector3(0, 1, 0);
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S)&&transform.position.y-moveSpeed>lowerBounds)
             movement += new Vector3(0, -1, 0);
 
-        if (transform.position.y + movement.y > upperBound)
-            movement.y = upperBound-movement.y;
         return movement;
     }
 }
