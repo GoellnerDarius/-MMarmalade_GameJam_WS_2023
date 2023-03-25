1c3f6f3c64a18b0e0950787d266febb37c8ce115
Felix Beer <mail@felix.beer>
2023-03-25 01:09:37 +0100

    Add Button handlers and second view to UI

--- Assets/UI/FlirtSwipe.cs
+++ Assets/UI/FlirtSwipe.cs
@@ -1,43 +1,56 @@
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class FlirtSwipe : MonoBehaviour
 {
     private Button _button;
-    private Toggle _toggle;
-
-    private int _clickCount;
+    //private Toggle _toggle;
 
     //Add logic that interacts with the UI controls in the `OnEnable` methods
     private void OnEnable()
     {
         // The UXML is already instantiated by the UIDocument component
         var uiDocument = GetComponent<UIDocument>();
+        
+        _button = uiDocument.rootVisualElement.Q<Button>("match");
+        _button.RegisterCallback<ClickEvent, VisualElement>(MatchClicked, uiDocument.rootVisualElement);
 
-        _button = uiDocument.rootVisualElement.Q("button") as Button;
-        _toggle = uiDocument.rootVisualElement.Q("toggle") as Toggle;
+        _button = uiDocument.rootVisualElement.Q<Button>("skip");
+        _button.RegisterCallback<ClickEvent, VisualElement>(SkipClicked, uiDocument.rootVisualElement);
+
+        // TODO: Create one Base FlirtScreen cs File and Load the other views dynamically.
+        // _button.UnregisterCallback<ClickEvent>(SkipClicked);
+        /*_toggle = uiDocument.rootVisualElement.Q("toggle") as Toggle;
 
         _button.RegisterCallback<ClickEvent>(PrintClickMessage);
 
         var _inputFields = uiDocument.rootVisualElement.Q("input-message");
-        _inputFields.RegisterCallback<ChangeEvent<string>>(InputMessage);
+        _inputFields.RegisterCallback<ChangeEvent<string>>(InputMessage);*/
+    }
+
+    private void MatchClicked(ClickEvent evt, VisualElement root)
+    {
+
+        Debug.Log("match clicked!");
+    }
+
+    private void SkipClicked(ClickEvent evt, VisualElement root)
+    {
+        Debug.Log("skip clicked!");
     }
 
     private void OnDisable()
     {
-        _button.UnregisterCallback<ClickEvent>(PrintClickMessage);
+        // _button.UnregisterCallback<ClickEvent>(PrintClickMessage);
     }
 
     private void PrintClickMessage(ClickEvent evt)
     {
-        ++_clickCount;
-
-        Debug.Log($"{"button"} was clicked!" +
-                (_toggle.value ? " Count: " + _clickCount : ""));
+ 
     }
 
     public static void InputMessage(ChangeEvent<string> evt)
     {
-        Debug.Log($"{evt.newValue} -> {evt.target}");
     }
 }
