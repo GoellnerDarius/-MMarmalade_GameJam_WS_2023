45a9323d7fd89ff03e209b6cbeb03af191ba70bf
Felix Beer <mail@felix.beer>
2023-03-26 17:03:14 +0200

    Change fonts and add characters

--- Assets/UI/FlirtMessages.cs
+++ Assets/UI/FlirtMessages.cs
@@ -4,6 +4,7 @@ using UnityEngine.UIElements;
 using System.IO;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEngine.Tilemaps;
 
 public class FlirtMessages : MonoBehaviour
 {
@@ -23,6 +24,8 @@ public class FlirtMessages : MonoBehaviour
     public System.Random random = new System.Random();
 
     public int temperature = 50;
+
+    public bool isFollowup = false;
 
     public Label typingIndicator;
 
@@ -44,9 +47,11 @@ public class FlirtMessages : MonoBehaviour
         document = GetComponent<UIDocument>();
 
         // INIT UI
-        document.rootVisualElement.Q<Image>("profile-picture").style.backgroundImage = new StyleBackground(flame.Images[1]);
+        ChangeFace(Reaction.neutral);
         document.rootVisualElement.Q<ProgressBar>("temperature").value = temperature;
         document.rootVisualElement.Q<VisualElement>("bubbles").style.opacity = 0;
+
+        SetTempImage("temp.png");
 
         SetNextDialog();
     }
@@ -61,10 +66,12 @@ public class FlirtMessages : MonoBehaviour
             } while (prevDialogs.Contains(currentDialog));
             prevDialogs.Add(currentDialog);
             await AddQuestion(currentDialog.q);
+            ChangeFace(Reaction.neutral);
             UpdateAnswers(currentDialog.answers);
         }
         else
         {
+            Debug.Log("Flirting finished");
             // TODO: Start minigame with score
         }
     }
@@ -77,7 +84,7 @@ public class FlirtMessages : MonoBehaviour
         AnimateTyping();
         await WaitSecondsAsync(3);
         typingIndicator.style.display = DisplayStyle.None;
-        document.rootVisualElement.Q<VisualElement>("messages").Insert(1, label);
+        document.rootVisualElement.Q<VisualElement>("messages").Insert(2, label);
     }
 
     private void AnimateTyping()
@@ -90,7 +97,6 @@ public class FlirtMessages : MonoBehaviour
                      {
                          while (typingIndicator.style.display == DisplayStyle.Flex)
                          {
-                             Debug.Log(element);
                              element.style.opacity = 0.2f;
                              await WaitSecondsAsync(1f);
                              element.style.opacity = 0.5f;
@@ -102,7 +108,6 @@ public class FlirtMessages : MonoBehaviour
     private async Task WaitSecondsAsync(float seconds)
     {
         await Task.Delay(TimeSpan.FromSeconds(seconds));
-        Debug.Log("Finished waiting.");
     }
 
     public void AddAnswer(string answer)
@@ -110,12 +115,11 @@ public class FlirtMessages : MonoBehaviour
         Label label = new Label();
         label.AddToClassList("right");
         label.text = answer;
-        document.rootVisualElement.Q<VisualElement>("messages").Insert(1, label);
+        document.rootVisualElement.Q<VisualElement>("messages").Insert(2, label);
     }
 
     public void UpdateAnswers(Answer[] answers)
     {
-        Debug.Log("Update answers");
         document.rootVisualElement.Q<VisualElement>("bubbles").style.opacity = 1;
         document.rootVisualElement.Q<Button>("a1").UnregisterCallback<ClickEvent, Answer>(AnswerClicked);
         document.rootVisualElement.Q<Button>("a2").UnregisterCallback<ClickEvent, Answer>(AnswerClicked);
@@ -135,33 +139,78 @@ public class FlirtMessages : MonoBehaviour
         AddAnswer(answer.a);
         if(answer.mood == 1 || answer.mood == 0)
         {
-            if(answer.mood == 0 && answer.followup != null) {
+            if(answer.mood == 0 && isFollowup) {
+                Debug.Log("medium");
                 temperature += 5;
             } else { 
                 temperature += 10;
+                if(answer.mood == 1) {
+                    Debug.Log("happy");
+                    ChangeFace(Reaction.happy);
+                }
             }
         } else
         {
             temperature -= 10;
+            ChangeFace(Reaction.angry);
+            await WaitSecondsAsync(2);
+            ChangeFace(Reaction.neutral);
         }
         UpdateTemperature();
+        Debug.Log(answer.followup);
+        Debug.Log(answer.followup.q);
         if (answer.mood == 1 && answer.followup != null && answer.followup.q != null)
         {
-            Debug.Log(answer.followup);
-            Debug.Log(answer.followup.q);
+            Debug.Log("follow up");
+            isFollowup = true;
             await AddQuestion(answer.followup.q);
+            ChangeFace(Reaction.neutral);
             UpdateAnswers(answer.followup.answers);
         } else
         {
+            Debug.Log("next question");
+            isFollowup = false;
             SetNextDialog();
         }
     }
 
     private void UpdateTemperature()
     {
-        Debug.Log("Temperature:" + temperature);
         document.rootVisualElement.Q<ProgressBar>("temperature").value = temperature >= 100 ? 99 : (temperature <= 0 ? 1 : temperature);
-        // TODO FIX values over 100 and under 0 because the progress bar isnt properly filled
+        if (temperature <= 100) {
+            SetTempImage("temp.png");
+        } else
+        {
+            SetTempImage("temp_max.png");
+        }
+    }
+
+    private void SetTempImage(string filename)
+    {
+        byte[] fileData;
+        fileData = File.ReadAllBytes("Assets/UI/Images/" + filename);
+        Texture2D tex = new Texture2D(2, 2);
+        tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        document.rootVisualElement.Q<IMGUIContainer>("temp").style.backgroundImage = tex;
+    }
+
+    private void ChangeFace(Reaction reaction)
+    {
+        int index = 0;
+        switch(reaction)
+        {
+            case Reaction.happy:
+                index = 0;
+                break;
+            case Reaction.neutral:
+                index = 1;
+                break;
+            case Reaction.angry:
+                index = 2;
+                break;
+        }
+        document.rootVisualElement.Q<Image>("profile-picture").style.backgroundImage = new StyleBackground(flame.Images[index]);
+
     }
 }
 [System.Serializable]
@@ -177,4 +226,11 @@ public class Answer
     public string a;
     public int mood;
     public Dialog followup;
+}
+
+public enum Reaction
+{
+    angry,
+    neutral,
+    happy
 }
